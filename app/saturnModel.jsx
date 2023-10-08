7adf676dacbebbf36adc1707640564cbd11e23d0
arysttoto <aryst.toto@gmail.com>
2023-10-08 22:47:57 +0600

    loading state for saturn

--- app/saturnModel.jsx
+++ app/saturnModel.jsx
@@ -1,22 +1,25 @@
 'use client'
 import "@google/model-viewer";
+import React, { Suspense } from 'react';
 
 function SaturnModel() {
   return (
-    <>
-    <div id="card"> 
-      <model-viewer
-        src="/saturn_model.glb"
-        ios-src=""
-        alt="A 3D model of Saturn"
-        shadow-intensity="1"
-        camera-controls
-        auto-rotate
-        ar
-        ar-modes="webxr scene-viewer quick-look"
-        style={{ width: '300px', height: '300px' }}
-      ></model-viewer> 
-    </div>
+    <> 
+    <Suspense fallback={<div>Loading...</div>}>
+      <div id="card"> 
+        <model-viewer
+          src="/saturn_model.glb"
+          ios-src=""
+          alt="A 3D model of Saturn"
+          shadow-intensity="1"
+          camera-controls
+          auto-rotate
+          ar
+          ar-modes="webxr scene-viewer quick-look"
+          style={{ width: '300px', height: '300px' }}
+        ></model-viewer> 
+      </div>
+    </Suspense>
     </>
   ); 
 }
